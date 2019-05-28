302de358f818ab35a5056220ea78ebaa78784c54
locoxsoco <a20151110@pucp.pe>
2019-05-28 16:52:29 -0500

    Build 03

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vue-argon-dashboard/precache-manifest.68e461bbd6ad2008810fcc25d37541be.js"
+  "precache-manifest.a307fbe884d3d3d6ab9fca320815397f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
