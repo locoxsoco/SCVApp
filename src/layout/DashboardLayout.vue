9bbfe43840938b5dbe29a9335ba019cd56355dea
locoxsoco <a20151110@pucp.pe>
2019-06-04 01:11:08 -0500

    Update sidebar

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -6,120 +6,120 @@
       title="Argon"
     >
       <template slot="links">
-        <sidebar-item
+        <sidebar-item v-if="['inicio'].indexOf($route.name) > -1"
           :link="{
             name: 'SCV',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-plane-arrival text-primary',
             path: '/inicio'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['menu','agregarUsuario','modificarUsuario','eliminarUsuario','agregarAviones','agregarTipoAviones','modificarAviones','modificarTipoAvionesPrincipal','modificarTipoAviones','eliminarAviones','eliminarTipoAviones','visualizarMapa','puertasYZonas'].indexOf($route.name) > -1"
           :link="{
             name: 'SCV',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-plane-arrival text-primary',
             path: '/admin/menu'
           }"
         />
-        <sidebar-item 
+        <sidebar-item v-if="['menu'].indexOf($route.name) > -1"
           :link="{
             name: 'Administración de usuario',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fa fa-users text-primary',
             path: '/admin/usuario/agregar'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['agregarUsuario','modificarUsuario','eliminarUsuario'].indexOf($route.name) > -1"
           :link="{
             name: 'Agregar Usuarios',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fa fa-user-plus text-primary',
             path: '/admin/usuario/agregar'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['agregarUsuario','modificarUsuario','eliminarUsuario'].indexOf($route.name) > -1"
           :link="{
             name: 'Modificar Usuarios',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fa fa-user text-primary',
             path: '/admin/usuario/modificar'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['agregarUsuario','modificarUsuario','eliminarUsuario'].indexOf($route.name) > -1"
           :link="{
             name: 'Eliminar Usuarios',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fa fa-user-times text-primary',
             path: '/admin/usuario/eliminar'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['menu'].indexOf($route.name) > -1"
           :link="{
             name: 'Administración de aviones y sus tipos',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-plane text-primary',
             path: '/admin/aviones/agregar'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['agregarAviones','agregarTipoAviones','modificarAviones','modificarTipoAvionesPrincipal','modificarTipoAviones','eliminarAviones','eliminarTipoAviones'].indexOf($route.name) > -1"
           :link="{
             name: 'Agregar Aviones',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-plus text-primary',
             path: '/admin/aviones/agregar'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['agregarAviones','agregarTipoAviones','modificarAviones','modificarTipoAvionesPrincipal','modificarTipoAviones','eliminarAviones','eliminarTipoAviones'].indexOf($route.name) > -1"
           :link="{
             name: 'Agregar Tipo de Aviones',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-plus text-primary',
             path: '/admin/aviones/agregarTipo'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['agregarAviones','agregarTipoAviones','modificarAviones','modificarTipoAvionesPrincipal','modificarTipoAviones','eliminarAviones','eliminarTipoAviones'].indexOf($route.name) > -1"
           :link="{
             name: 'Modificar Aviones',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-edit text-primary',
             path: '/admin/aviones/modificar'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['agregarAviones','agregarTipoAviones','modificarAviones','modificarTipoAvionesPrincipal','modificarTipoAviones','eliminarAviones','eliminarTipoAviones'].indexOf($route.name) > -1"
           :link="{
             name: 'Modificar Tipo de Aviones',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-edit text-primary',
             path: '/admin/aviones/modificarTipo'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['agregarAviones','agregarTipoAviones','modificarAviones','modificarTipoAvionesPrincipal','modificarTipoAviones','eliminarAviones','eliminarTipoAviones'].indexOf($route.name) > -1"
           :link="{
             name: 'Eliminar Aviones',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-eraser text-primary',
             path: '/admin/aviones/eliminar'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['agregarAviones','agregarTipoAviones','modificarAviones','modificarTipoAvionesPrincipal','modificarTipoAviones','eliminarAviones','eliminarTipoAviones'].indexOf($route.name) > -1"
           :link="{
             name: 'Eliminar Tipo de Aviones',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-eraser text-primary',
             path: '/admin/aviones/eliminarTipo'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['menu'].indexOf($route.name) > -1"
           :link="{
             name: 'Administración de zonas y puertas',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-parking text-primary',
             path: '/admin/puertasYZonas'
           }"
         />
-        <sidebar-item
+        <sidebar-item v-if="['menu'].indexOf($route.name) > -1"
           :link="{
             name: 'Visualizar el mapa',
-            icon: 'ni ni-tv-2 text-primary',
+            icon: 'fas fa-map text-primary',
             path: '/admin/visualizarMapa'
           }"
         />
         
 
-        <sidebar-item :link="{name: 'Icons', icon: 'ni ni-planet text-blue', path: '/icons'}"/>
+        <!--<sidebar-item :link="{name: 'Icons', icon: 'ni ni-planet text-blue', path: '/icons'}"/>
         <sidebar-item :link="{name: 'Maps', icon: 'ni ni-pin-3 text-orange', path: '/maps'}"/>
         <sidebar-item :link="{name: 'User Profile', icon: 'ni ni-single-02 text-yellow', path: '/profile'}"/>
         <sidebar-item :link="{name: 'Tables', icon: 'ni ni-bullet-list-67 text-red', path: '/tables'}"/>
         <sidebar-item :link="{name: 'Login', icon: 'ni ni-key-25 text-info', path: '/login'}"/>
         <sidebar-item :link="{name: 'Register', icon: 'ni ni-circle-08 text-pink', path: '/register'}"/> 
-
+        -->
       </template>
     </side-bar>
     <div class="main-content" :data="sidebarBackground">
