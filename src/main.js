ab0e721fb695233e97a400002c7a7d855769a1f9
Patricio <patricio.avila@pucp.pe>
2019-06-21 16:37:12 -0500

    mapa

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import router from './router'
 import './registerServiceWorker'
 import ArgonDashboard from './plugins/argon-dashboard'
 import axios from 'axios'
+import VueKonva from 'vue-konva'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 Vue.use(ArgonDashboard)
 Vue.use(Buefy)
+Vue.use(VueKonva)
 new Vue({
   router,
   render: h => h(App)
